aa759f042b04295527d43f52c5588d12f5cd13bb
Camilo Monsalve Gómez <beren.monsalve@gmail.com>
2025-05-10 20:24:58 +0000

    fix on things

--- n8n_agents/core/wsgi.py
+++ n8n_agents/core/wsgi.py
@@ -8,6 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Handle zoneinfo compatibility before Django imports
+if sys.version_info >= (3, 9):
+    try:
+        import zoneinfo
+    except ImportError:
+        pass
+else:
+    try:
+        import backports.zoneinfo
+    except ImportError:
+        pass
 
 from django.core.wsgi import get_wsgi_application
 
